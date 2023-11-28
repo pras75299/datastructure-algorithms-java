c4b9abdb63b9d504299886e8c6652312a4e1238b
pras75299 <singh.prashantking@gmail.com>
2023-11-28 23:42:25 +0530

    insert in a linked list question description and basic structure

---
+++ problems/insertInLinkedList.java
@@ -0,0 +1,14 @@
+/*You are given A which is the head of a linked list. Also given is the value B and position C. 
+Complete the function that should insert a new node with the said value at the given position
+
+Notes: 
+1-In case the position is more than length of linked list, simply insert the new node at the tail only.
+2-In case the pos is 0, simple insert the new node at the head only
+3-Follow 0-based indexing for the node numbering
+*/
+
+public class Solution{
+    public ListNode solve(ListNode A, int B, int C){
+
+    }
+}
